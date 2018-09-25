452976144e04a13ebd288a37354b97ffb396f899
ahmetsen347 <ahmetsen347@gmail.com>
2018-09-25 17:34:03 +0300

    Fifth Commit For Automatic Jenkins Build;

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -3,6 +3,7 @@ public class LoginPage {
     private static String loginName = "Deneme";
     private static String loginPass = "Deneme2";
     private static String btnSubmit = "Deneme3";
+    private static String btnClick = "Deneme4";
 
 
 }
