d1d444e0e3dda6eae1d8b6d3400d3efd167b9c74
ahmetsen347 <ahmetsen347@gmail.com>
2018-09-26 11:24:04 +0300

    LoginPage.java removed from project;

--- src/main/java/LoginPage.java
+++
@@ -1,9 +0,0 @@
-public class LoginPage {
-
-    private static String loginName = "Deneme";
-    private static String loginPass = "Deneme2";
-    private static String btnSubmit = "Deneme3";
-    private static String btnClick = "Deneme4";
-
-
-}
